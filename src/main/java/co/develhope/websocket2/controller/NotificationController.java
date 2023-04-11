5fd61473f9467a94685aa521a1269676b3850b02
Giuseppe Albanese <giuse00876alba@hotmail.com>
2023-04-11 18:17:53 +0200

    Initial commit

---
+++ src/main/java/co/develhope/websocket2/controller/NotificationController.java
@@ -0,0 +1,38 @@
+package co.develhope.websocket2.controller;
+
+import co.develhope.websocket2.entities.ClientMessageDTO;
+import co.develhope.websocket2.entities.MessageDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NotificationController {
+
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    @PostMapping("/broadcast-message")
+    public ResponseEntity sendNotificationToClients(@RequestBody MessageDTO messageDTO){
+        simpMessagingTemplate.convertAndSend("/topic/broadcast", messageDTO);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+
+    @MessageMapping("/message")
+    @SendTo("/client-message")
+    public MessageDTO sendMessage(ClientMessageDTO clientMessageDTO){
+        //MessageDTO messageDTO = new MessageDTO();
+        //messageDTO.setSender(clientMessageDTO.getClientName());
+        //messageDTO.setType(clientMessageDTO.getClientAlert());
+        //messageDTO.setMessage(clientMessageDTO.getClientMsg());
+        return new MessageDTO("sender "+ clientMessageDTO.getClientName(),clientMessageDTO.getClientAlert(), clientMessageDTO.getClientMsg());
+    }
+
+}
